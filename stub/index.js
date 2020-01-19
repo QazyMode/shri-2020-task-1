0fccf9d754ae683ebd4dd82c45fd6362fcec4100
QazyMode <matsekha.aleks@yahoo.com>
2020-01-19 19:39:16 +0300

    0.2

--- stub/index.js
+++ stub/index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const {bemhtml} = require('bem-xjst');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const { bemhtml } = require("bem-xjst");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
 const templates = bemhtml.compile();
 
-app.use('/static', express.static(`${__dirname}/build`));
+app.use("/static", express.static(`${__dirname}/../build`));
 
-app.get('/index|product', (req, res) => {
-    const page = req.path.replace('/', '');
+app.get("/index|product", (req, res) => {
+  const page = req.path.replace("/", "");
 
-    const jsonPath = path.resolve(__dirname, 'pages', `${page}.json`);
-    const bemjson = JSON.parse(fs.readFileSync(jsonPath, {encoding: 'utf-8'}));
+  const jsonPath = path.resolve(__dirname, "pages", `${page}.json`);
+  const bemjson = JSON.parse(fs.readFileSync(jsonPath, { encoding: "utf-8" }));
 
-    const contentHTML = templates.apply(bemjson);
+  const contentHTML = templates.apply(bemjson);
 
-    const pageHTML = `
+  const pageHTML = `
     <!doctype html>
     <html>
         <head>
@@ -27,11 +27,11 @@ app.get('/index|product', (req, res) => {
             ${contentHTML}
             <script type="text/javascript" src="/static/script.js"></script>
         </body>
-    </html>`
+    </html>`;
 
-    res.send(pageHTML);
+  res.send(pageHTML);
 });
 
 app.listen(3000, () => {
-    console.log('App is listening on port 3000');
-})
+  console.log("App is listening on port 3000");
+});
